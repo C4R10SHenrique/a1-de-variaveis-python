3d96ea71be17616da787c8502156c9de315e3a9e
C4R10SHenrique <car4los007@gmail.com>
2024-09-06 15:31:30 -0500

    projeto 1

---
+++ main.py
@@ -0,0 +1,8 @@
+name = input("Digite seu nome: ")
+address = input("Digite o seu endereço: ")
+number = input("Digite o seu número: ")
+salary = input("Digite o seu salário: ")
+qualities = input("Digite algumas qualidades separadas por vírgula: ")
+
+text = f"Olá, meu nome é {name}, moro em {address}, meu número de contato é {number}, e atualmente estou recebendo {salary}, algumas de minhas habilidades são: {qualities}."
+print(text)
